1123b61c07bef97d095ca1be3f67c1be86fcefbb
fabioseixas <dev.fabioseixas@gmail.com>
2024-01-24 18:08:02 -0300

    feat: 3 new case tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
 import { faker } from '@faker-js/faker';
 
+function createContact(params) {
+  const name = faker.person.firstName();
+  cy.get('input[placeholder="Nome *"]').type(name);
+  if (params) {
+    const email = params.email;
+    cy.get('input[placeholder="Email"]').type(email);
+  }
+  cy.get('button[type=submit]').click();
+  return name;
+}
+
 describe('template spec', () => {
   beforeEach(() => {
     // reset the database prior to every test
@@ -12,15 +23,15 @@ describe('template spec', () => {
     cy.contains('Novo Contato').click();
 
     cy.url({ timeout: 3000 }).should('include', 'new');
+
+    cy.contains('Cadastrar').should('exist');
   });
 
   it('[Criação]: criar contato e ver novo contato na listagem', () => {
     cy.visit('http://localhost:3000/new');
 
-    const name = faker.person.firstName();
-    cy.get('input[placeholder="Nome *"]').type(name);
+    const name = createContact();
 
-    cy.get('button[type=submit]').click();
     cy.get('a[href="/"]').click();
 
     cy.contains(name);
@@ -31,16 +42,12 @@ describe('template spec', () => {
     // assert
     cy.contains(name);
   });
+
   it('[Edição]: criar contatos e editar contato recém criado', () => {
     // create 2 contacts
     cy.visit('http://localhost:3000/new');
-    const name = faker.person.firstName();
-    cy.get('input[placeholder="Nome *"]').type(name);
-    cy.get('button[type=submit]').click();
-
-    const name2 = faker.person.firstName();
-    cy.get('input[placeholder="Nome *"]').type(name2);
-    cy.get('button[type=submit]').click();
+    const name = createContact();
+    createContact();
 
     // back to home
     cy.get('a[href="/"]').click();
@@ -75,13 +82,8 @@ describe('template spec', () => {
   it('[Delete]: criar contatos e deletar um contato', () => {
     // create 2 contacts
     cy.visit('http://localhost:3000/new');
-    const name = faker.person.firstName();
-    cy.get('input[placeholder="Nome *"]').type(name);
-    cy.get('button[type=submit]').click();
-
-    const name2 = faker.person.firstName();
-    cy.get('input[placeholder="Nome *"]').type(name2);
-    cy.get('button[type=submit]').click();
+    const name = createContact();
+    const name2 = createContact();
 
     // back to home
     cy.get('a[href="/"]').click();
@@ -105,7 +107,47 @@ describe('template spec', () => {
     cy.contains(name2).should('exist');
   });
 
+  it('[Busca]: criar contatos e buscar corretamente', () => {
+    // create 2 contacts
+    cy.visit('http://localhost:3000/new');
+
+    const name1 = createContact();
+    const name2 = createContact();
+
+    // back to home
+    cy.get('a[href="/"]').click();
+
+    // escrever nome no campo de busca
+    cy.get('input[placeholder="Pesquisa pelo nome..."]').type(name1);
+
+    cy.contains(name1).should('exist');
+    cy.contains(name2).should('not.exist');
+  });
+  it('[Toast]: sucesso ao criar contato', () => {
+    // create contact
+    cy.visit('http://localhost:3000/new');
+
+    createContact();
+
+    // assert success Toast
+    cy.contains('Contato cadastrado com sucesso').should('exist');
+  });
+  it.only('[Toast]: erro ao criar contato com mesmo email', () => {
+    // create 2 contacts with same email
+    cy.visit('http://localhost:3000/new');
+
+    createContact({ email: 'test@mail.com' });
+
+    // dimiss success toast
+    cy.contains('Contato cadastrado com sucesso').click();
+
+    createContact({ email: 'test@mail.com' });
+
+    // assert success Toast
+    cy.contains('Ocorreu um erro ao cadastrar o contato!').should('exist');
+    cy.contains('Contato cadastrado com sucesso').should('not.exist');
+  });
+
   it('[Ordenação]: criar contatos e ordenar corretamente (asc)', () => {});
   it('[Ordenação]: criar contatos e ordenar corretamente (desc)', () => {});
-  it('[Busca]: criar contatos e buscar corretamente', () => {});
 });
